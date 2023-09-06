098f0b525db5c032c9420ca03c0ade93325597b3
mahdi mehrabi <mahdimehrabi17@gmail.com>
2023-09-06 23:23:06 +0330

    add: handle successes and errors

--- service-producer/kafka.go
+++ service-producer/kafka.go
@@ -9,10 +9,10 @@ import (
 )
 
 var allowToProduce = false
-var kafka = NewKafkaProducer([]string{"localhost:29092", "localhost:29093"})
+var kafka = NewKafkaProducer([]string{"localhost:29092", "localhost:39092"})
 var queueMutex = sync.Mutex{}
 
-func Producer(topic string, partition int, wg *sync.WaitGroup) {
+func Producer(topic string, partition int32, wg *sync.WaitGroup) {
 	defer func() {
 		recover()
 		wg.Done()
@@ -28,8 +28,9 @@ func Producer(topic string, partition int, wg *sync.WaitGroup) {
 
 		//produce to kafka
 		kafka.Input() <- &sarama.ProducerMessage{
-			Topic: topic,
-			Value: sarama.ByteEncoder(data),
+			Topic:     topic,
+			Value:     sarama.ByteEncoder(data),
+			Partition: partition,
 		}
 		fmt.Println("sent ", string(data), "to kafka asynchronously")
 		//remove if produced successfully to kafka
@@ -37,6 +38,8 @@ func Producer(topic string, partition int, wg *sync.WaitGroup) {
 }
 
 func ProduceKafka() {
+	go handleSuccesses()
+	go handleErrors()
 	for {
 		Cond.L.Lock()
 		if !allowToProduce {
@@ -63,36 +66,40 @@ func NewKafkaProducer(brokersList []string) sarama.AsyncProducer {
 	config.Producer.Retry.Max = 10
 	config.Producer.Flush.Frequency = 500 * time.Millisecond // Flush batches every 500ms
 	config.Producer.Compression = sarama.CompressionSnappy   // Compress messages
+	config.Producer.Return.Successes = true
+	config.Producer.Return.Errors = true
 
 	producer, err := sarama.NewAsyncProducer(brokersList, config)
 	if err != nil {
 		log.Fatalln("Failed to start Sarama producer:", err)
 	}
 
-	go func() {
-		for err := range producer.Errors() {
-			//logging out to STDOUT
-			log.Println("Warning: failed to produce:", err)
-
-			//adding message to queue again on error
-			byts, er := err.Msg.Value.Encode()
-			if er != nil {
-				fmt.Println("Critical: failed to encode message:", err.Msg, "we lost it,err:", er.Error())
-				continue
-			}
-			Queue.Add(byts)
-		}
-	}()
-	go func() {
-		for message := range producer.Successes() {
-			byts, err := message.Value.Encode()
-			if err != nil {
-				fmt.Println("Warning: failed to encode message:", err.Error())
-				continue
-			}
-			//adding message to redis on successful storing on kafka
-			log.Println("successfully produced:", string(byts))
-		}
-	}()
 	return producer
+}
+
+func handleSuccesses() {
+	for message := range kafka.Successes() {
+		byts, err := message.Value.Encode()
+		if err != nil {
+			fmt.Println("Warning: failed to encode message:", err.Error())
+			continue
+		}
+		//adding message to redis on successful storing on kafka
+		log.Println("successfully produced:", string(byts))
+	}
+}
+
+func handleErrors() {
+	for err := range kafka.Errors() {
+		//logging out to STDOUT
+		log.Println("Warning: failed to produce:", err)
+
+		//adding message to queue again on error
+		byts, er := err.Msg.Value.Encode()
+		if er != nil {
+			fmt.Println("Critical: failed to encode message:", err.Msg, "we lost it,err:", er.Error())
+			continue
+		}
+		Queue.Add(byts)
+	}
 }
