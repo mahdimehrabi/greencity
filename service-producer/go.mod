1f7801079648d9569079c515067492faf0d773b2
mahdi mehrabi <mahdimehrabi17@gmail.com>
2023-09-06 18:43:18 +0330

    Update: queue

---
+++ service-producer/go.mod
@@ -0,0 +1,5 @@
+module greencity
+
+go 1.21.0
+
+require github.com/eapache/queue v1.1.0 // indirect
--- service-producer/queue.go
+++ service-producer/queue.go
@@ -1,15 +1,25 @@
 package main
 
-import "github.com/eapache/queue"
+import (
+	"github.com/eapache/queue"
+	"sync"
+)
 
+var Cond = sync.NewCond(&sync.Mutex{})
 var Queue = queue.New()
 
 func FillQueue() {
 	for {
-		byts, err := GenerateRandomBytes(15)
+		byts, err := GenerateRandomBytes(321)
 		if err != nil {
 			panic(err.Error())
 		}
 		Queue.Add(byts)
+		if Queue.Length()%1024 == 0 {
+			Cond.L.Lock()
+			allowToProduce = true
+			Cond.Broadcast()
+			Cond.L.Unlock()
+		}
 	}
 }
